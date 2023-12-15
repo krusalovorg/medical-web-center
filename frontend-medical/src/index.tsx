616d7db2b4eb7be2c15cc049265fdfb2ff87fc8b
Egor Dudkin <krusalovpro@gmail.com>
2023-12-15 16:59:20 +0400

    up

--- frontend-medical/src/index.tsx
+++ frontend-medical/src/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const {
+  createBrowserRouter,
+  RouterProvider,
+} = require("react-router-dom");
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "*",
+    element: <App/>
+  }
+]);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'anonymous'} />
+      <link href="/output.css" rel="stylesheet" />
+      {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" /> */}
+    </head>
+    <RouterProvider router={router} />
+    {/* <Footer/> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend-medical/src/utils/backend.tsx
@@ -0,0 +1,37 @@
+export const URL_SERVER = "http://127.0.0.0.1:5000"
+
+export async function getData() {
+    let url = URL_SERVER;
+    try {
+        console.log('fetch url:', url)
+        const response = await fetch(url, { method: "GET", headers: { 'Content-Type': 'application/json' } });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        // return data as any;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+async function requestData(url: string) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+}
+
+export async function getItemById(id: string, category: "places" | "routes" | "category") {
+    try {
+        const url = `${URL_SERVER}/get_details_id?id=${id}&table_name=${category}`;
+        console.log('fetch url:', url)
+        const data = await requestData(url);
+        //console.log('data:', data) 
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
