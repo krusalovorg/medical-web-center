62d0228b26bfd65ea0cd72163e7aa547ca280c85
Misha <134804106+cheked@users.noreply.github.com>
2023-12-16 00:25:10 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from datetime import timedelta
 import os
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 cluster = MongoClient("mongodb://localhost:27017")
 accounts_db = cluster["accounts"]
@@ -17,6 +19,7 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
 jwt = JWTManager(app)
 
 
+# add something to data base
 def add_to_database(data, db):
     if db == 'accounts':
         collection_db.insert_one(data)
@@ -24,6 +27,7 @@ def add_to_database(data, db):
         reference_db.insert_one(data)
 
 
+# password from  data base
 def find_in_database(email=None, phone_number=None):
     if email:
         user = collection_db.find_one({"email": email})
@@ -34,34 +38,48 @@ def find_in_database(email=None, phone_number=None):
         if user:
             return user['password']
 
+
+def get_user():
+    if collection_db.find_one({"email": get_jwt_identity()}):
+        result = collection_db.find_one({"email": get_jwt_identity()})
+        return result
+    elif collection_db.find_one({"phone_number": get_jwt_identity()}):
+        result = collection_db.find_one({"phone_number": get_jwt_identity()})
+        return result
+
+
+# sign up
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
     name = data['name']
     surname = data['surname']
     patronymic = data['patronymic']
-    password = data['password']
-    phone_number = data.get('phone_number','')
+    password = generate_password_hash(data['passsword'], method='sha256')
+    phone_number = data.get('phone_number', '')
     email = data['email']
-    birthday = data.get('birthday','')
+    avatar = os.path.join(app.root_path, 'images', 'user.svg')
+    birthday = data.get('birthday', '')
     position = data.get("position", "")
-    user_type = data['user_type']
-
+    isDoctor = data['isDoctor']
+    expirience = data['expirience']
+    place=data['place']
     add_to_database(
         {'name': name, 'surname': surname, 'patronymic': patronymic, 'password': password, 'phone_number': phone_number,
          'email': email,
-         'birthday': birthday, 'position': position, 'user_type': user_type}, 'accounts')
+         'birthday': birthday, 'position': position, 'user_type': isDoctor, 'avatar': avatar, 'expirience': expirience,'place':place},
+        'accounts')
     return jsonify({'message': 'User registered successfully'})
 
 
-# Получение входных данных пользователя
+# log in
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     password = data['password']
     if data['email']:
         email = data['email']
-        if find_in_database(email=email) == password:
+        if find_in_database(email=email) == generate_password_hash(password, method='sha256'):
             access_token = create_access_token(identity=email)
             print(access_token, 'token')
             return jsonify(access_token=access_token), 200
@@ -69,7 +87,7 @@ def login():
             return jsonify({'message': 'incorrect password'})
     elif data['phone_number']:
         phone_number = data['phone_number']
-        if find_in_database(phone_number=phone_number) == password:
+        if find_in_database(phone_number=phone_number) == generate_password_hash(password, method='sha256'):
             access_token = create_access_token(identity=phone_number)
             print(access_token, 'token')
             return jsonify(access_token=access_token), 200
@@ -79,6 +97,7 @@ def login():
         return jsonify({'message': 'no email or phone_number'})
 
 
+# add reference
 @app.route('/add_references', methods=['POST'])
 @jwt_required()
 def add_reff():
@@ -102,6 +121,7 @@ def add_reff():
     return jsonify({'message': 'добавленно в таблицу '})
 
 
+# show references
 @app.route('/show_references', methods=['POST'])
 @jwt_required()
 def show_ref():
@@ -112,12 +132,13 @@ def show_ref():
     return jsonify({result})
 
 
+# show doctors
 @app.route('/show_doctor', methods=['POST'])
 @jwt_required()
 def show_doctor():
     result = []
     data = request.get_json()
-    search_item = data.get('search_item','')
+    search_item = data.get['search_item']
     user = collection_db.find({
         "$or": [
             {"surname": {"$regex": search_item, "$options": "i"}},
@@ -127,22 +148,28 @@ def show_doctor():
         ]
     })
     for document in user:
-        document['_id'] = str(document['_id'])
         result.append(document)
-    print(result)
-    return jsonify(result)
+    return jsonify({result})
 
 
+# get user by key
 @app.route('/get_user_by_key', methods=['POST'])
 @jwt_required()
 def get_user():
-    if collection_db.find_one({"email": get_jwt_identity()}):
-        result = collection_db.find_one({"email": get_jwt_identity()})
-        return jsonify({result})
-    elif collection_db.find_one({"phone_number": get_jwt_identity()}):
-        result = collection_db.find_one({"phone_number": get_jwt_identity()})
-        return jsonify({result})
+    return jsonify({get_user()})
 
 
+@app.route('/update_user', methods=['POST'])
+@jwt_required()
+def update_by_id():
+    data = request.get_json()
+    user = get_user()['_id']
+    document = collection_db.find_one({'_id': user})
+    for key in data.keys():
+        document[key] = data[key]
+    collection_db.update_one({'_id': user}, {'$set': document})
+
+
+# start program
 if __name__ == '__main__':
     app.run()
